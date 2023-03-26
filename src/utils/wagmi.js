bab9920797a2fc4a67acab7f123357a852c365b2
vrajdesai78 <vrajdesai78@gmail.com>
2023-03-26 18:50:34 +0530

    multi-chain support added

--- src/utils/wagmi.js
+++ src/utils/wagmi.js
@@ -1,17 +1,48 @@
 import { createClient } from "wagmi";
 import { getDefaultClient } from 'connectkit';
+import { optimism } from "@wagmi/core/chains";
 
-const firechain = {
-  id: 997,
-  name: "5ire",
-  network: "5ireChain",
+const mantle = {
+  id: 5001,
+  name: "Mantle",
+  network: "Mantle Testnet",
   nativeCurrency: {
     decimals: 18,
-    name: "5ire",
-    symbol: "5ire",
+    name: "BIT",
+    symbol: "BIT",
   },
   rpcUrls: {
-    default: { http: ["https://rpc-testnet.5ire.network"] }
+    default: { http: ["https://rpc.testnet.mantle.xyz"] }
+  },
+  testnet: true,
+};
+
+const zkevm = {
+  id: 1442,
+  name: "zkEVM",
+  network: "zkEVM-testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "ETH",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: { http: ["https://rpc.public.zkevm-test.net"] }
+  },
+  testnet: true,
+};
+
+const scroll = {
+  id: 534353,
+  name: "scroll",
+  network: "scrollAlpha",
+  nativeCurrency: {
+    decimals: 18,
+    name: "scrollAlpha",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: { http: ["https://alpha-rpc.scroll.io/l2"] },
   },
   testnet: true,
 };
@@ -19,7 +50,6 @@ const firechain = {
 export const client = createClient(
   getDefaultClient({
     autoConnect: true,
-    appName: 'FitQuest',
-    chains: [firechain],
-  })
-);
+    appName: 'TokenVerse',
+    chains: [mantle, optimism, zkevm, scroll],
+  }));
